eb23a28612419d87060addc5a1ba72658cdff423
Agrovoltic <thrindanava@gmail.com>
2024-12-13 23:03:33 +0530

    Increased sampling rate with RTOs

--- MPU6050_mqtt/src/main.cpp
+++ MPU6050_mqtt/src/main.cpp
@@ -5,11 +5,11 @@
 #include <PubSubClient.h>
 
 // Replace with your network credentials and MQTT broker details
-const char* ssid = "Eng-Student";//"Tharinda_Lap";//"TIEC-2.4G";//"SLT-Fiber-37DB";
-const char* password = "3nG5tuDt";//"Rashmi1213";//"nava@123";//"TIEC2954";
-const char* mqtt_server = "10.30.9.146";//"192.168.1.105";//"192.168.1.2";//"192.168.1.102";//"ec2-54-219-34-11.us-west-1.compute.amazonaws.com";
-const char* mqtt_topic = "sensor/mpu6050_01";
-const char* client_id = "ESP32_MPU6050_01";
+const char* ssid = "SLT-Fiber-37DB";//"Eng-Student";//"Tharinda_Lap";//"TIEC-2.4G";//"SLT-Fiber-37DB";
+const char* password = "nava@123";//"3nG5tuDt";//"Rashmi1213";//"nava@123";//"TIEC2954";
+const char* mqtt_server = "192.168.1.2";//"192.168.1.105";//"192.168.1.2";//"192.168.1.10";//"ec2-54-219-34-11.us-west-1.compute.amazonaws.com";
+const char* mqtt_topic = "sensor/mpu6050_02";
+const char* client_id = "ESP32_MPU6050_02";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -36,6 +36,9 @@ void dmpDataReady() {
   mpuInterrupt = true;
 }
 
+SemaphoreHandle_t dataSemaphore;
+String jsonPayload;
+
 // MQTT reconnect function
 void reconnect() {
   while (!client.connected()) {
@@ -51,13 +54,69 @@ void reconnect() {
   }
 }
 
+void getSensorDataTask(void* parameter) {
+  while (true) {
+    if (!dmpReady) continue;
+
+    if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      mpu.dmpConvertToWorldFrame(&aaWorld, &aaReal, &q);
+      mpu.dmpGetEuler(euler, &q);
+
+      acc_X = aaReal.x / 835.066;
+      acc_Y = aaReal.y / 835.066;
+      acc_Z = aaReal.z / 835.066;
+
+      unsigned long timestamp = millis(); // Get timestamp in milliseconds
+
+      xSemaphoreTake(dataSemaphore, portMAX_DELAY);
+      
+      xSemaphoreGive(dataSemaphore);
+    }
+
+    delay(5);  // Minimize task blocking
+  }
+}
+
+void publishSensorDataTask(void* parameter) {
+  while (true) {
+    if (!client.connected()) {
+      reconnect();
+    }
+    client.loop();
+
+    xSemaphoreTake(dataSemaphore, portMAX_DELAY);
+    unsigned long timestamp = millis(); // Get timestamp in milliseconds
+
+    jsonPayload = "{";
+    jsonPayload += "\"timestamp\":\"" + String(timestamp) + "\",";
+    jsonPayload += "\"acc_X\":\"" + String(acc_X) + "\",";
+    jsonPayload += "\"acc_Y\":\"" + String(acc_Y) + "\",";
+    jsonPayload += "\"acc_Z\":\"" + String(acc_Z) + "\",";
+    jsonPayload += "\"w\":\"" + String(q.w) + "\",";
+    jsonPayload += "\"i\":\"" + String(q.x) + "\",";
+    jsonPayload += "\"j\":\"" + String(q.y) + "\",";
+    jsonPayload += "\"k\":\"" + String(q.z) + "\",";
+    jsonPayload += "\"psi\":\"" + String(euler[0]) + "\",";
+    jsonPayload += "\"theta\":\"" + String(euler[1]) + "\",";
+    jsonPayload += "\"phi\":\"" + String(euler[2]) + "\"";
+    jsonPayload += "}";
+    client.publish(mqtt_topic, jsonPayload.c_str());
+    xSemaphoreGive(dataSemaphore);
+
+    delay(15);  // Adjust as needed for data frequency
+  }
+}
+
 void setup() {
   Serial.begin(9600);
 
   // Setup WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    WiFi.begin(ssid, password);
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
@@ -73,14 +132,6 @@ void setup() {
 
   devStatus = mpu.dmpInitialize();
 
-  // Supply your own gyro offsets here
-  // mpu.setXGyroOffset(23);
-  // mpu.setYGyroOffset(-54);
-  // mpu.setZGyroOffset(-4);
-  // mpu.setXAccelOffset(-946); 
-  // mpu.setYAccelOffset(-116);
-  // mpu.setZAccelOffset(1226);
-
   if (devStatus == 0) {
     mpu.CalibrateAccel(6);
     mpu.CalibrateGyro(6);
@@ -91,55 +142,15 @@ void setup() {
   } else {
     Serial.println("MPU6050 initialization failed");
   }
-}
 
-void getSensorData() {
-  if (!dmpReady) return;
+  // Create semaphore
+  dataSemaphore = xSemaphoreCreateMutex();
 
-  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
-    mpu.dmpGetQuaternion(&q, fifoBuffer);
-    mpu.dmpGetAccel(&aa, fifoBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    mpu.dmpConvertToWorldFrame(&aaWorld, &aaReal, &q);
-    mpu.dmpGetEuler(euler, &q);
-
-    acc_X = aaReal.x / 835.066;
-    acc_Y = aaReal.y / 835.066;
-    acc_Z = aaReal.z / 835.066;
-  }
-}
-
-void publishSensorData() {
-  unsigned long timestamp = millis(); // Get timestamp in milliseconds
-
-  String jsonPayload ="{";
-  jsonPayload += "\"timestamp\":\"" + String(timestamp) + "\",";
-  jsonPayload += "\"acc_X\":\""+ String(acc_X) + "\",";
-  jsonPayload += "\"acc_Y\":\""+ String(acc_Y) + "\",";
-  jsonPayload += "\"acc_Z\":\""+ String(acc_Z) + "\",";
-  jsonPayload += "\"w\":\""+ String(q.w) + "\",";
-  jsonPayload += "\"i\":\""+ String(q.x) + "\",";
-  jsonPayload += "\"j\":\""+ String(q.y) + "\",";
-  jsonPayload += "\"k\":\""+ String(q.z) + "\",";
-  jsonPayload += "\"psi\":\""+ String(euler[0]) + "\",";
-  jsonPayload += "\"theta\":\""+ String(euler[1]) + "\",";
-  jsonPayload += "\"phi\":\""+ String(euler[2]) + "\"";
-  jsonPayload += "}";
-  
-  client.publish(mqtt_topic, jsonPayload.c_str());
-  // Serial.println("Published data: ");
-  // Serial.println(jsonPayload);
+  // Create tasks for each core
+  xTaskCreatePinnedToCore(getSensorDataTask, "GetSensorDataTask", 4096, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(publishSensorDataTask, "PublishSensorDataTask", 4096, NULL, 1, NULL, 1);
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
-
-  getSensorData();
-  publishSensorData();
-
-  delay(85);  // Adjust as needed for data frequency
+  // Loop is empty since tasks handle the workload
 }
